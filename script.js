f78a73759ef87ddeab3ad908893d3d1baf96234c
ThibautMilville <tmilville.pro@gmail.com>
2023-05-03 21:17:21 +0200

    easier comment

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ stars.forEach((star, index1) => {
   star.addEventListener("click", () => {
     // ADD THE ACTIVE CLASS TO THE STAR THAT WAS CLICKED AND ALL THE STARS BEFORE IT
     stars.forEach((star, index2) => {
-      // IF THE INDEX OF THE CLICKED STAR IS <= TO THE INDEX OF THE CURRENT STAR IN THE LOOP, ADD THE ACTIVE CLASS
-      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+      index2 <= index1 ? star.classList.add("active") : star.classList.remove("active");
     })
   })
 })
